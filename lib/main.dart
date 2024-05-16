8754a4d1fedf1b1308c4af4860eb203cb6053d98
Vishnu Priyan S S <vishnu32510@gmail.com>
2024-05-16 10:02:50 +0530

    add barrel file for theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'package:code_store/modules/authentication/signup_bloc/signup_bloc.dart';
 import 'package:code_store/modules/authentication/wrappers/auhentication_wrapper.dart';
 import 'package:code_store/modules/authentication/authentication_bloc/authentication_bloc.dart';
 import 'package:code_store/modules/authentication/wrappers/authentication_listner_wrapper.dart';
-import 'package:code_store/modules/theme/theme_bloc/theme_bloc.dart';
-import 'package:code_store/modules/theme/theme_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'modules/theme/theme.dart';
 
 void main() {
   /// `runApp(const MyApp());` is the entry point of a Flutter application. It tells Flutter to start
---
+++ lib/modules/theme/theme.dart
@@ -0,0 +1,2 @@
+export 'theme_wrapper.dart';
+export 'theme_bloc/theme_bloc.dart';
