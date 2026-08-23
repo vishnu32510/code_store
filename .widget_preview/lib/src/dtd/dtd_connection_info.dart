4debba8da05ac26839b138e0dde59de6c8013c5b
Vishnu Priyan S S <vishnu32510@gmail.com>
2026-08-22 20:30:25 -0400

    feat: implement widget preview scaffold with custom inspector service for IDE navigation

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:63485/fExvwB9YjDY=';
+const String kWidgetPreviewService =
+    'widget-preview-b349253d-e810-446f-a3b9-f10962a36bc6';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-b349253d-e810-446f-a3b9-f10962a36bc6';
+const String kProjectRootPath = r'/Users/vishnu/garage/code_store';
