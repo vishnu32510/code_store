00a20e021bc58bf847d1e411f38442c16c7719c7
Vishnu Priyan S S <vishnu32510@gmail.com>
2026-08-24 20:35:35 -0400

    chore: update DTD connection configuration and upgrade project dependencies

--- .widget_preview/lib/src/dtd/dtd_connection_info.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: implementation_imports
-
-const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:60956/H8eal9BHw6Q=';
-const String kWidgetPreviewService =
-    'widget-preview-8db33caa-3866-4318-8ce2-3231193461a2';
-const String kWidgetPreviewScaffoldStream =
-    'WidgetPreviewScaffold-8db33caa-3866-4318-8ce2-3231193461a2';
-const String kProjectRootPath = r'/Users/vishnu/garage/code_store';
